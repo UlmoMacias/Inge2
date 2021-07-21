66d65c98894d75e6bfdb0fb665d3870a8fd65267
jacs04x <30944431+jacs04x@users.noreply.github.com>
2021-07-21 14:05:35 -0500

    creacion de componentes, modelos y services

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FacturaComponent } from './_models/factura/factura.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
+import { RegionComponent } from './components/region/region.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacturaComponent,
+    ClienteComponent,
+    RegionComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductosComponent,
+    ArticulosComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
