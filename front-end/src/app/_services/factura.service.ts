973ae4cb2cc0f84d5933bd907f1e9356f5bff24f
jacs04x <30944431+jacs04x@users.noreply.github.com>
2021-07-21 14:26:05 -0500

    impl model and service of factura

--- front-end/src/app/_services/factura.service.ts
+++ front-end/src/app/_services/factura.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import {Factura} from '../_models/factura';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacturaService {
 
-  constructor() { }
+  API_URI = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {   }
+
+//hay que checar esto
+
+  getFacturas(){
+    return this.http.get(this.API_URI+"/facturas")
+  }
+
+  getFactura(id: number){
+    return this.http.get(this.API_URI+"/factura/"+id)
+  }
+
+  createFactura(factura: Factura){
+    return this.http.post(this.API_URI+"/factura", factura)
+  }
+
+  editFactura(factura: Factura){
+    return this.http.put(this.API_URI+"/factura/"+factura.id, factura)
+  }
+
+  deleteFactura(id: number){
+    return this.http.delete(this.API_URI+"/factura/"+id);
+  }  
+
 }
