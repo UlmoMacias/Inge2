b5a2358a0f0761dba369d0f494c585ad81e9720e
Dafne Miranda <dafnemiranda@MacBook-Pro-de-Dafne.local>
2021-08-03 21:13:54 -0500

    About-Factura

--- front-end/src/app/components/articulos/articulos.component.ts
+++ front-end/src/app/components/articulos/articulos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Articulo } from 'src/app/_models/articulo';
+import { ArticulosService } from 'src/app/_services/articulos.service'
 
 declare var $: any
 
@@ -9,10 +10,36 @@ declare var $: any
   styleUrls: ['./articulos.component.css']
 })
 export class ArticulosComponent implements OnInit {
-
-  constructor() { }
+  
+  articulos : Articulo [] | any
+  constructor(
+    private articulosService: ArticulosService) { };
 
   ngOnInit(): void {
+    this.getArticulos()
+  };
+
+  //
+  // id: number
+  // cantidad: number
+  // codigo: String
+  // impuesto: number
+  // precio_unitario: number
+  // subtotal: number
+  // total: number
+  // id_factura: number
+  getArticulos() {
+    this.articulos = [new Articulo(1, 2, "codigo1", 0.23, 15, 30, 30.23, 1), new Articulo(2, 3, "codigo2", 0.32, 20, 40, 40.32, 2) ]
+    //    this.regionService.getRegiones().subscribe(
+    //      res=>{
+    //        this.regiones = res
+    //      },
+    //      err=>{
+    //        console.error(err)
+    //      }
+    //
+    //    )
   }
+
 
 }
--- front-end/src/app/components/factura/factura.component.ts
+++ front-end/src/app/components/factura/factura.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {Factura} from 'src/app/_models/factura'
-import {FacturaService} from 'src/app/_services/factura.service'
-import {Producto } from '../../_models/producto'
-import {Region} from '../../_models/region'
-import {Cliente} from '../../_models/cliente'
-import {Articulo} from '../../_models/articulo'
+import { Factura } from 'src/app/_models/factura'
+import { FacturaService } from 'src/app/_services/factura.service'
+import { Producto } from '../../_models/producto'
+import { Region } from '../../_models/region'
+import { Cliente } from '../../_models/cliente'
+import { Articulo } from '../../_models/articulo'
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import {ActivatedRoute } from '@angular/router'
+import { ActivatedRoute } from '@angular/router'
 import { ProductosService } from 'src/app/_services/productos.service';
 
 
@@ -19,53 +19,53 @@ declare var $: any
 })
 export class FacturaComponent implements OnInit {
   rfc: string;
-  facturas : Factura [] | any
-  articulos : Articulo [] | any
-  productos : Producto [] | any
+  facturas: Factura[] | any
+  articulos: Articulo[] | any
+  productos: Producto[] | any
 
   constructor(private route: ActivatedRoute, private facturaService: FacturaService, private productosService: ProductosService) { }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => { 
-      this.rfc = params.rfc; 
-    });   
+    this.route.queryParams.subscribe(params => {
+      this.rfc = params.rfc;
+    });
     this.getFacturas()
   }
-  getFacturas(){
-    
-  this.facturas = [new Factura(1, new Date("11 08 2019 ") , this.rfc, 12, 234, 123.45, 5656, [new Articulo(1,123,"codigo", 34, 56, 1,12, 123 ),new Articulo(2,123,"codigo1", 34, 56, 1,12, 123 ),new Articulo(3,123,"codigo2", 34, 56, 1,12, 123 )  ])]
-//    this.facturaService.getFacturas(this.rfc).subscribe(
-//      res => {
-//        this.facturas = res
-//      },
-//      err => {
-//        console.error(err)
-//      }
-//    )
+  getFacturas() {
+
+    this.facturas = [new Factura(1, new Date("11 08 2019 "), this.rfc, 12, 234, 123.45, 5656, [new Articulo(1, 123, "codigo", 34, 56, 1, 12, 123), new Articulo(2, 123, "codigo1", 34, 56, 1, 12, 123), new Articulo(3, 123, "codigo2", 34, 56, 1, 12, 123)])]
+    //    this.facturaService.getFacturas(this.rfc).subscribe(
+    //      res => {
+    //        this.facturas = res
+    //      },
+    //      err => {
+    //        console.error(err)
+    //      }
+    //    )
 
   }
 
-  eliminaFactura(id_factura : number){
-    console.log("eliminaFactura: "+ id_factura)
+  eliminaFactura(id_factura: number) {
+    console.log("eliminaFactura: " + id_factura)
   }
 
-  editarFactura(id_factura: number){
-    console.log("editar factura: "+ id_factura)
+  editarFactura(id_factura: number) {
+    console.log("editar factura: " + id_factura)
   }
 
 
-  getProductos(){
+  getProductos() {
     this.productos = [new Producto(1, "codigo", "nombre", "describe", 1, 2, new Date("12 12 1997"), 1)]
 
   }
 
-  detalleArticulos(id_factura: number){
-  for (let a of this.facturas){
-      if (id_factura == a.id ){
-          this.articulos = a.articulos
-          
-        }
+  detalleArticulos(id_factura: number) {
+    for (let a of this.facturas) {
+      if (id_factura == a.id) {
+        this.articulos = a.articulos
+
       }
+    }
 
     $("#nuevo").modal({
       backdrop: 'static'
@@ -73,17 +73,17 @@ export class FacturaComponent implements OnInit {
     $("#nuevo").modal("show")
   }
 
-  detallesProducto(codigo:String ){
-      this.productos = [new Producto(1, "codigo", "nombre", "describe", 1, 2, new Date("12 12 1997"), 1)]
-//    this.productos.getProducto(codigo).subscribe(
-//      res => {
-//        this.productos = res
-//      },
-//      err => {
-//        console.error(err)
-//      }
-//    )
-    
+  detallesProducto(codigo: String) {
+    this.productos = [new Producto(1, "codigo", "nombre", "describe", 1, 2, new Date("12 12 1997"), 1)]
+    //    this.productos.getProducto(codigo).subscribe(
+    //      res => {
+    //        this.productos = res
+    //      },
+    //      err => {
+    //        console.error(err)
+    //      }
+    //    )
+
 
     $("#nuevo").modal("hide")
 
