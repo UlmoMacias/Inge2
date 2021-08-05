afc4d79c195379410a092b8b839d334fc7ace5b7
Dafne Miranda <daphs_mirandas@ciencias.unam.mx>
2021-08-05 18:24:24 -0500

    Update art

--- front-end/src/app/components/articulos/articulos.component.ts
+++ front-end/src/app/components/articulos/articulos.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Articulo } from 'src/app/_models/articulo';
-import { Producto } from 'src/app/_models/producto';
+import { Factura } from 'src/app/_models/factura';
+import { Region } from 'src/app/_models/region';
 import { ArticulosService } from 'src/app/_services/articulos.service';
 import { ProductosService } from 'src/app/_services/productos.service';
 import { ProductosComponent } from '../productos/productos.component';
 import { FormGroup, FormBuilder } from '@angular/forms'
 import { ThrowStmt } from '@angular/compiler';
+
+import { FacturaService } from 'src/app/_services/factura.service';
 
 declare var $: any
 
@@ -17,9 +20,11 @@ declare var $: any
 export class ArticulosComponent implements OnInit {
   
   articulos : Articulo [] | any
-  carrito : Producto [] = []; 
+  factura : Factura [] | any
+  carrito : Articulo [] = []; 
   productos : Producto [] | any
   carritoForm : FormGroup | any
+  submitted = false
   constructor(
     private articulosService: ArticulosService, private formBuilder: FormBuilder) { };
 
@@ -55,7 +60,8 @@ export class ArticulosComponent implements OnInit {
     //    )
   }
   getProductos(){
-    this.productos = [new Producto(1, "codigo2", "nombre", "descripcion", 1, 100, new Date("03 08 2021"), 12)]
+    this.productos = [new Producto(1, "codigo2", "nombre", "descripcion", 1, 100, new Date("03 08 2021"), 12),
+                      new Producto(2, "codigo3", "nombrasde", "desacripcion", 1, 100, new Date("03 08 2021"), 12)]
 
   }
   aniadir(producto: Producto){
@@ -72,7 +78,62 @@ export class ArticulosComponent implements OnInit {
 
   obtenerCantidad(){
     console.log("Este es el obtener Cantidad")
+    console.log(JSON.stringify(this.carritoForm.value));
+    this.submitted = true
+    if (this.carritoForm.invalid) {
+      console.log("Formulario invalido")
+      return
+    }
+
+    // Se parsea el JSON a lista de Articulos
+    this.carrito = [new Articulo(1, 2, "codigo1", 0.23, 15, 30, 30.23, 1)]
+
+    //
+
+
+
+
+    
+    
+    this.submitted = false
+    $("#nuevo").modal("hide")
+    this.showSucces("Agregado!")
+    
+
+
+
+    //    this.CarritoService.createCarrito(newCarrito).subscribe(
+      //      res =>{
+        //
+        //        this.submitted=false
+        //        $("#nuevo").modal("hide")
+        //        this.showSucces("Agregado!")
+        //        this.getCarritos()
+        //      },
+        //      err => {
+
+          //        this.showFail("Algo salio mal ")
+          //
+          //      }
+          //    )
 
   }
+
+
+
+
+
+
+
+
+
+
+
+  crear_FActura(listaObjetos,listaCAntidad)
+  sadjasdjsda
+
+  add_Factura(Factura)
+
+
 
 }
